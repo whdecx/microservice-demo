64a98f23b1fceac3cd4d793bcbc4dd5393185288
Xun Cao <xuncaoxc@gmail.com>
2025-11-10 12:36:29 -0800

    first commit

---
+++ src/main/java/org/example/microservice_demo/MicroserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.microservice_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDemoApplication.class, args);
+	}
+
+}
